09d35934ab58c7c865aee516651a9f1d6f795c68
ThisisYoYoDev <yoel.edery@epitech.eu>
2022-06-28 05:52:20 +0200

    Add: V1 of vector c template and update README.md

--- README.md
+++ README.md
@@ -1,2 +1,390 @@
 # C-vector-template
 template in c for vector
+
+
+## How to install
+Just drop the `vector.h` header file in your project.
+
+### Macro definitions
+
+#### **CREATE_VECTOR**
+*CREATE_VECTOR allow you to initialize the structure of your vector.*
+
+Example:
+```c
+
+typedef struct {
+    int x;
+    int y;
+} point_t;
+
+CREATE_VECTOR(point, point_t);
+```
+point_t = type of the vector
+point = name of the struct
+The type of the vector here is ```vector_point_t```
+
+#### **VECTOR_INIT**
+*VECTOR_INIT allow you to initialize the variable of the vector and to allocate it.*
+VECTOR_INIT has optional parameters either you specify a size of your choice or the size will be that of the BUFFER (128).
+
+Example:
+```
+void example_vector(void)
+{
+    VECTOR_INIT(point, v1);
+    VECTOR_INIT(point, v2, 50);
+}
+```
+point = name of the struct (declare in CREATE_VECTOR)
+v1 | v2 = created variable
+50 = capacity of the vector
+
+#### **VECTOR_RESERVE**
+*VECTOR_RESERVE aKa VECTOR_RESIZE*
+VECTOR_RESERVE allow you to realloc your vector for given size.
+
+Example:
+```c
+void example_vector(void)
+{
+    VECTOR_INIT(point, v1);
+
+    VECTOR_RESERVE(v1, 50);
+}
+```
+v1 = variable of the vector
+50 = add 50 elements to the capacity of the vector
+
+
+#### **VECTOR_PUSH_BACK**
+*VECTOR_PUSH_BACK: Push a value at the end of a vector.*
+VECTOR_PUSH_BACK can take one or more element to add in the vector.
+
+Example:
+```c
+void example_vector(void)
+{
+    VECTOR_INIT(point, v1);
+
+    VECTOR_PUSH_BACK(v1, (point_t){1, 1});
+    /* or */
+    VECTOR_PUSH_BACK(v1, (point_t){10, 10}, (point_t){5, 5}, (point_t){-999, 999});
+}
+```
+
+
+#### **VECTOR_PUSH_AT**
+*VECTOR_PUSH_AT: Push a value at a given index.*
+Same usage that VECTOR_PUSH_BACK but at a given index.
+
+Example:
+```c
+void example_vector(void)
+{
+    VECTOR_INIT(point, v1);
+
+    VECTOR_PUSH_BACK(v1, (point_t){1, 1});
+    /* or */
+    VECTOR_PUSH_BACK(v1, (point_t){10, 10}, (point_t){5, 5}, (point_t){-999, 999});
+
+    VECTOR_PUSH_AT(v1, 0, (point_t){1, 1});
+    /* or */
+    VECTOR_PUSH_AT(v1, 3, (point_t){10, 10}, (point_t){5, 5}, (point_t){-999, 999});
+}
+```
+3 = index to push the element
+
+#### **VECTOR_PUSH_FRONT**
+*VECTOR_PUSH_FRONT: Push a value at the front of a vector*
+Same usage that VECTOR_PUSH_BACK.
+
+Example:
+```c
+void example_vector(void)
+{
+    VECTOR_INIT(point, v1);
+
+    VECTOR_PUSH_FRONT(v1, (point_t){1, 1});
+    /* or */
+    VECTOR_PUSH_FRONT(v1, (point_t){10, 10}, (point_t){5, 5}, (point_t){-999, 999});
+}
+```
+
+#### **VECTOR_POP_BACK**
+*VECTOR_POP_BACK: Pop a value at the end of a vector.*
+
+Example:
+```c
+void example_vector(void)
+{
+    VECTOR_INIT(point, v1);
+
+    VECTOR_PUSH_FRONT(v1, (point_t){1, 1});
+    VECTOR_POP_BACK(v1);
+}
+```
+
+#### **VECTOR_POP_FRONT**
+*VECTOR_POP_FRONT: Pop a value at the front of a vector.*
+
+Example:
+```c
+void example_vector(void)
+{
+    VECTOR_INIT(point, v1);
+
+    VECTOR_PUSH_FRONT(v1, (point_t){1, 1});
+    VECTOR_POP_FRONT(v1);
+}
+```
+
+#### **VECTOR_POP_AT**
+*VECTOR_POP_AT: Pop a value at a given index.*
+
+Example:
+```c
+void example_vector(void)
+{
+    VECTOR_INIT(point, v1);
+
+    VECTOR_PUSH_FRONT(v1, (point_t){1, 1});
+    VECTOR_PUSH_FRONT(v1, (point_t){1, 1});
+    VECTOR_PUSH_FRONT(v1, (point_t){1, 1});
+    VECTOR_PUSH_FRONT(v1, (point_t){1, 1});
+    VECTOR_POP_AT(v1, 2);
+}
+```
+2 = index to pop
+
+#### **VECTOR_AT**
+*VECTOR_AT: Get a value at a given index.*
+
+Example:
+```c
+void example_vector(void)
+{
+    VECTOR_INIT(point, v1);
+
+    VECTOR_PUSH_FRONT(v1, (point_t){1, 1});
+    printf("{%d,%d}", VECTOR_AT(v1, 0).x, VECTOR_AT(v1, 0).y);
+}
+```
+
+#### **VECTOR_SIZE**
+*VECTOR_SIZE: Get the size of a vector.*
+
+Example:
+```c
+void example_vector(void)
+{
+    VECTOR_INIT(point, v1);
+
+    VECTOR_PUSH_FRONT(v1, (point_t){1, 1});
+    printf("Size: %ld\n", VECTOR_SIZE(v1));
+}
+```
+
+#### **VECTOR_CAPACITY**
+*VECTOR_CAPACITY: Get the capacity of a vector.*
+
+Example:
+```c
+void example_vector(void)
+{
+    VECTOR_INIT(point, v1);
+
+    VECTOR_PUSH_FRONT(v1, (point_t){1, 1});
+    printf("Size: %ld\n", VECTOR_CAPACITY(v1));
+}
+```
+
+#### **VECTOR_FIRST**
+*VECTOR_FIRST: Get the first value of a vector.*
+
+Example:
+```c
+void example_vector(void)
+{
+    VECTOR_INIT(point, v1);
+
+    VECTOR_PUSH_FRONT(v1, (point_t){1, 1});
+    printf("FIRST: %d\n", VECTOR_FIRST(v1).x);
+}
+```
+
+#### **VECTOR_LAST**
+*VECTOR_LAST: Get the first value of a vector.*
+
+Example:
+```c
+void example_vector(void)
+{
+    VECTOR_INIT(point, v1);
+
+    VECTOR_PUSH_FRONT(v1, (point_t){1, 1});
+    printf("LAST: %d\n", VECTOR_LAST(v1).y);
+}
+```
+
+#### **VECTOR_BEGIN**
+*VECTOR_BEGIN: Get the begin iterator of a vector*
+
+#### **VECTOR_END**
+*VECTOR_END: Get the end iterator of a vector*
+
+#### **VECTOR_CLEAR**
+*VECTOR_CLEAR: Clear a vector (set size to 0)*
+this macro does not destroy the vector.
+
+
+#### **VECTOR_FOREACH**
+*VECTOR_FOREACH: Iterate over a vector.*
+
+Example:
+```c
+void example_vector(void)
+{
+    VECTOR_INIT(point, v1);
+
+    VECTOR_PUSH_FRONT(v1, (point_t){1, 1}, (point_t){2, 2}, (point_t){3, 3}, (point_t){4, 4});
+    VECTOR_FOREACH(v_int, i) {
+        printf("{%d,%d}", VECTOR_AT(v1, i).x, VECTOR_AT(v1, i).y);
+    }
+}
+```
+
+#### **VECTOR_FOREACH_AUTO**
+*VECTOR_FOREACH_AUTO: Iterate over a vector and give pointer to type.*
+
+Example:
+```c
+void example_vector(void)
+{
+    VECTOR_INIT(point, v1);
+
+    VECTOR_PUSH_FRONT(v1, (point_t){1, 1}, (point_t){2, 2}, (point_t){3, 3}, (point_t){4, 4});
+    VECTOR_FOREACH_AUTO(v_int, i) {
+        printf("{%d,%d}", i->x, i->y);
+    }
+}
+```
+
+#### **VECTOR_FOREACH_AUTO_BACK**
+*VECTOR_FOREACH_AUTO_BACK: Iterate over a vector in reverse order and give pointer to type.*
+same usage that VECTOR_FOREACH_AUTO
+
+#### **VECTOR_FOREACH_BACKWARD**
+*VECTOR_FOREACH_BACKWARD: Iterate over a vector in reverse order.*
+same usage that VECTOR_FOREACH
+
+#### **VECTOR_SWAP**
+*VECTOR_SWAP: Swap two values at two given indices.*
+
+Example:
+```c
+void example_vector(void)
+{
+    VECTOR_INIT(point, v1);
+
+    VECTOR_PUSH_FRONT(v1, (point_t){1, 1}, (point_t){2, 2}, (point_t){3, 3}, (point_t){4, 4});
+    VECTOR_SWAP(v1, 0, 2);
+}
+```
+
+#### **VECTOR_MERGE**
+*VECTOR_MERGE: Merge two vectors.*
+
+Example:
+```c
+void example_vector(void)
+{
+    VECTOR_INIT(point, v1);
+    VECTOR_INIT(point, v2);
+
+    VECTOR_PUSH_FRONT(v1, (point_t){1, 1}, (point_t){2, 2}, (point_t){3, 3}, (point_t){4, 4});
+    VECTOR_PUSH_FRONT(v2, (point_t){1, 1}, (point_t){2, 2}, (point_t){3, 3}, (point_t){4, 4});
+    VECTOR_MERGE(v1, v2);
+}
+```
+v1 = dest
+v2 = src
+
+#### **VECTOR_ERASE_IF**
+*VECTOR_ERASE_IF: Erase a value (by index) if a condition is valid.*
+
+Example:
+```c
+void example_vector(void)
+{
+    VECTOR_INIT(point, v1);
+
+    VECTOR_PUSH_BACK(v1, (point_t){1, 1}, (point_t){2, 2}, (point_t){3, 3}, (point_t){4, 4});
+    bool toto = true;
+    printf("AT 3: %d\n", VECTOR_AT(v_int, 3).x);
+    VECTOR_ERASE_IF(v_int, 3, toto == true);
+    printf("AT 3: %d\n", VECTOR_AT(v_int, 3).x);
+}
+```
+
+#### **VECTOR_ERASE_ALL_IF**
+*VECTOR_ERASE_ALL_IF: Erase all values if a condition is valid.*
+Example:
+```c
+int compare(int a, int b)
+{
+    return What you want;
+}
+
+void example_vector(void)
+{
+    VECTOR_INIT(point, v1);
+
+    VECTOR_PUSH_BACK(v1, (point_t){1, 1}, (point_t){2, 2}, (point_t){3, 3}, (point_t){4, 4});
+    bool toto = true;
+    printf("AT 3: %d\n", VECTOR_AT(v_int, 3).x);
+    VECTOR_ERASE_ALL_IF(v_int, 3, toto == true, compare);
+    printf("AT 3: %d\n", VECTOR_AT(v_int, 3).x);
+}
+```
+
+#### **VECTOR_SORT**
+*VECTOR_SORT: Sort values in a vector.*
+Example:
+```c
+int compare(int a, int b)
+{
+    return What you want;
+}
+
+void example_vector(void)
+{
+    VECTOR_INIT(point, v1);
+
+    VECTOR_PUSH_BACK(v1, (point_t){1, 1}, (point_t){2, 2}, (point_t){3, 3}, (point_t){4, 4});
+    VECTOR_SORT(v1, compare);
+}
+```
+
+#### **VECTOR_DELETE_ARRAY**
+*VECTOR_DELETE_ARRAY: Delete a vector array.*
+
+#### **DELETE_VECTOR**
+*DELETE_VECTOR: Delete a vector.*
+
+Example:
+```c
+int compare(int a, int b)
+{
+    return What you want;
+}
+
+void example_vector(void)
+{
+    VECTOR_INIT(point, v1);
+
+    VECTOR_PUSH_BACK(v1, (point_t){1, 1}, (point_t){2, 2}, (point_t){3, 3}, (point_t){4, 4});
+    VECTOR_SORT(v1, compare);
+    DELETE_VECTOR(v1);
+}
+```
---
+++ include/vector.h
@@ -0,0 +1,335 @@
+/*
+
+Vector C template
+
+Author:
+    Yoel Edery
+
+Compile with:
+    - gcc
+    - clang
+
+Tested compiler version:
+    - gcc: 11.2.0 (Ubuntu 11.2.0-19ubuntu1)
+    - clang: Ubuntu clang version 14.0.0-1ubuntu1
+
+*/
+
+#ifndef VECTOR_H_
+#define VECTOR_H_
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BUFFER 128
+
+#define MYERR(...)                                               \
+    do {                                                         \
+        printf("@ Vector Error: ./%s:%d: ", __FILE__, __LINE__); \
+        printf(__VA_ARGS__);                                     \
+    } while (0)
+
+/* CREATE_VECTOR MACRO: Example CREATE_VECTOR(name, type) */
+#define CREATE_VECTOR(name, type)      \
+    typedef struct vector_##name##_s { \
+        size_t size;                   \
+        size_t capacity;               \
+        type *array;                   \
+    } vector_##name##_t;
+
+/* VECTOR_INIT_SIZE MACRO: Initialize a vector with a given size */
+#define VECTOR_INIT_SIZE(vector, name_var, size)                     \
+    vector_##vector##_t *name_var = (vector_##vector##_t *)calloc(1, \
+    sizeof(vector_##vector##_t));                                    \
+    VECTOR_RESERVE(name_var, size);                                  \
+    if (!name_var) {                                                 \
+        MYERR("calloc failed\n");                                    \
+        exit(84);                                                    \
+    }
+
+#define VECTOR_INIT_DEFAULT(vector, name_var) \
+    VECTOR_INIT_SIZE(vector, name_var, BUFFER)
+
+#define GET_4TH_ARG(arg1, arg2, arg3, arg4, ...) arg4
+
+#define VECTOR_INIT_CHOOSER(...) \
+    GET_4TH_ARG(__VA_ARGS__, VECTOR_INIT_SIZE, VECTOR_INIT_DEFAULT, )
+
+/* VECTOR_INIT MACRO: Initialize a vector with default */
+/* buffer size or you can tell the size */
+#define VECTOR_INIT(...) VECTOR_INIT_CHOOSER(__VA_ARGS__)(__VA_ARGS__)
+
+/* VECTOR_RESERVE: Reserve a given size for a vector */
+#define VECTOR_RESERVE(vector, CAPACITY)                              \
+    do {                                                              \
+        vector->array = (typeof(vector->array))realloc(vector->array, \
+        (vector->capacity + CAPACITY) *                               \
+        sizeof(typeof(vector->array[0])));                            \
+        if (!vector->array) {                                         \
+            MYERR("realloc failed\n");                                \
+            exit(84);                                                 \
+        }                                                             \
+        vector->capacity += CAPACITY;                                 \
+    } while (0)
+
+#define NUMARGS(type, ...) \
+    (sizeof((typeof(type)[]){type, ##__VA_ARGS__}) / sizeof(typeof(type)) - 1)
+
+/* VECTOR_PUSH_BACK: Push a value at the end of a vector */
+#define VECTOR_PUSH_BACK(vector, ...)                             \
+    do {                                                          \
+        if (!vector) {                                            \
+            MYERR("vector is NULL\n");                            \
+            break;                                                \
+        }                                                         \
+        if (vector->size >= vector->capacity) {                   \
+            VECTOR_RESERVE(vector, BUFFER);                       \
+        }                                                         \
+        size_t num_args = NUMARGS(vector->array[0], __VA_ARGS__); \
+        for (size_t i = 0; i < num_args / BUFFER; i++) {          \
+            VECTOR_RESERVE(vector, BUFFER);                       \
+        }                                                         \
+        typeof(vector->array[0]) args[] = {__VA_ARGS__};          \
+        for (size_t i = 0; i < num_args; i++) {                   \
+            vector->array[vector->size] = args[i];                \
+            vector->size += 1;                                    \
+        }                                                         \
+    } while (0)
+
+/* VECTOR_PUSH_AT: Push a value at a given index */
+#define VECTOR_PUSH_AT(vector, index, ...)                               \
+    do {                                                                 \
+        if (!vector) {                                                   \
+            MYERR("vector is NULL\n");                                   \
+            break;                                                       \
+        }                                                                \
+        if ((index > (long)vector->size && vector->size) || index < 0) { \
+            MYERR("index out of range\n");                               \
+            exit(84);                                                    \
+        }                                                                \
+        if (vector->size >= vector->capacity) {                          \
+            VECTOR_RESERVE(vector, BUFFER);                              \
+        }                                                                \
+        size_t num_args = NUMARGS(vector->array[0], __VA_ARGS__);        \
+        typeof(vector->array[0]) args[] = {__VA_ARGS__};                 \
+        for (size_t i = 0; i < num_args / BUFFER; i++) {                 \
+            VECTOR_RESERVE(vector, BUFFER);                              \
+        }                                                                \
+        memmove(&vector->array[index + num_args],                        \
+        &vector->array[index],                                           \
+        (vector->size - index) * sizeof(vector->array[0]));              \
+        for (size_t i = 0; i < num_args; i++) {                          \
+            vector->array[index + i] = args[i];                          \
+            vector->size += 1;                                           \
+        }                                                                \
+    } while (0)
+
+/* VECTOR_PUSH_FRONT: Push a value at the front of a vector */
+#define VECTOR_PUSH_FRONT(vector, ...) \
+    VECTOR_PUSH_AT(vector, 0, __VA_ARGS__)
+
+/* VECTOR_POP_BACK: Pop a value at the end of a vector */
+#define VECTOR_POP_BACK(vector)         \
+    do {                                \
+        if (!vector) {                  \
+            MYERR("vector is NULL\n");  \
+            break;                      \
+        }                               \
+        if (vector->size == 0) {        \
+            MYERR("vector is empty\n"); \
+            exit(84);                   \
+        }                               \
+        --vector->size;                 \
+    } while (0)
+
+/* VECTOR_POP_FRONT: Pop a value at the front of a vector */
+#define VECTOR_POP_FRONT(vector)             \
+    do {                                     \
+        if (!vector) {                       \
+            MYERR("vector is NULL\n");       \
+            break;                           \
+        }                                    \
+        if (vector->size == 0) {             \
+            MYERR("vector is empty\n");      \
+            exit(84);                        \
+        }                                    \
+        PRIVATE_VECTOR_REMOVE_AT(vector, 0); \
+    } while (0)
+
+/* VECTOR_AT: Get a value at a given index */
+#define VECTOR_AT(vector, index) vector->array[index]
+
+/* VECTOR_SIZE: Get the size of a vector */
+#define VECTOR_SIZE(vector) vector->size
+
+/* VECTOR_FIRST: Get the first value of a vector */
+#define VECTOR_FIRST(vector) VECTOR_AT(vector, 0)
+
+/* VECTOR_LAST: Get the last value of a vector */
+#define VECTOR_LAST(vector) VECTOR_AT(vector, vector->size)
+
+/* VECTOR_CAPACITY: Get the capacity of a vector */
+#define VECTOR_CAPACITY(vector) vector->capacity
+
+/* PRIVATE_VECTOR_REMOVE_AT: Remove a value at a given index */
+#define PRIVATE_VECTOR_REMOVE_AT(vector, index)                   \
+    do {                                                          \
+        if (!vector) {                                            \
+            MYERR("vector is NULL\n");                            \
+            break;                                                \
+        }                                                         \
+        memmove(vector->array + index, vector->array + index + 1, \
+        (vector->size - index - 1) * sizeof(vector->array[0]));   \
+        --vector->size;                                           \
+    } while (0)
+
+/* VECTOR_POP_AT: Pop a value at a given index */
+#define VECTOR_POP_AT(vector, index)             \
+    do {                                         \
+        if (!vector) {                           \
+            MYERR("vector is NULL\n");           \
+            break;                               \
+        }                                        \
+        if (index >= vector->size) {             \
+            MYERR("index out of range\n");       \
+            exit(84);                            \
+        }                                        \
+        PRIVATE_VECTOR_REMOVE_AT(vector, index); \
+    } while (0)
+
+/* VECTOR_DELETE_ARRAY: Delete a vector array */
+#define VECTOR_DELETE_ARRAY(vector)          \
+    do {                                     \
+        if (!vector->array) {                \
+            MYERR("vector array is NULL\n"); \
+            break;                           \
+        }                                    \
+        free(vector->array);                 \
+        vector->array = NULL;                \
+    } while (0)
+
+/* DELETE_VECTOR: Delete a vector */
+#define VECTOR_DELETE(vector)          \
+    do {                               \
+        if (!vector) {                 \
+            MYERR("vector is NULL\n"); \
+            break;                     \
+        }                              \
+        VECTOR_DELETE_ARRAY(vector);   \
+        free(vector);                  \
+        vector = NULL;                 \
+    } while (0)
+
+/* VECTOR_BEGIN: Get the begin iterator of a vector */
+#define VECTOR_BEGIN(vector) vector->array
+
+/* VECTOR_END: Get the end iterator of a vector */
+#define VECTOR_END(vector) vector->array + vector->size
+
+/* VECTOR_CLEAR: Clear a vector (set size to 0) */
+#define VECTOR_CLEAR(vector)           \
+    do {                               \
+        if (!vector) {                 \
+            MYERR("vector is NULL\n"); \
+            break;                     \
+        }                              \
+        vector->size = 0;              \
+    } while (0)
+
+/* VECTOR_FOREACH: Iterate over a vector */
+#define VECTOR_FOREACH(vector, it) \
+    for (size_t it = 0; it < VECTOR_SIZE(vector); ++it)
+
+/* VECTOR_FOREACH_AUTO: Iterate over a vector and give pointer to type */
+#define VECTOR_FOREACH_AUTO(vector, it)                          \
+    for (typeof(VECTOR_BEGIN(vector)) it = VECTOR_BEGIN(vector); \
+         it != VECTOR_END(vector); ++it)
+
+/* VECTOR_FOREACH_AUTO_BACK: Iterate over a vector in reverse order */
+/* and give pointer to type */
+#define VECTOR_FOREACH_AUTO_BACK(vector, it)                     \
+    for (typeof(VECTOR_END(vector)) it = VECTOR_END(vector) - 1; \
+         it != VECTOR_BEGIN(vector); --it)
+
+/* VECTOR_FOREACH_BACKWARD: Iterate over a vector in reverse order */
+#define VECTOR_FOREACH_BACKWARD(vector, it) \
+    for (long it = VECTOR_SIZE(vector) - 1; it >= 0; --it)
+
+/* VECTOR_SWAP: Swap two values at two given indices */
+#define VECTOR_SWAP(vector, index1, index2)                     \
+    do {                                                        \
+        if (!vector) {                                          \
+            MYERR("vector is NULL\n");                          \
+            break;                                              \
+        }                                                       \
+        if (index1 >= vector->size || index2 >= vector->size) { \
+            MYERR("index out of range\n");                      \
+            exit(84);                                           \
+        }                                                       \
+        typeof(vector->array[0]) tmp = vector->array[index1];   \
+        vector->array[index1] = vector->array[index2];          \
+        vector->array[index2] = tmp;                            \
+    } while (0)
+
+/* VECTOR_MERGE: Merge two vectors */
+#define VECTOR_MERGE(vector1, vector2)                              \
+    do {                                                            \
+        if (!vector1 || !vector2) {                                 \
+            MYERR("vector is NULL\n");                              \
+            break;                                                  \
+        }                                                           \
+        if (vector1->capacity < vector1->size + vector2->size) {    \
+            VECTOR_RESERVE(vector1, vector1->size + vector2->size); \
+        }                                                           \
+        memcpy(vector1->array + vector1->size, vector2->array,      \
+        vector2->size * sizeof(vector2->array[0]));                 \
+        vector1->size += vector2->size;                             \
+    } while (0)
+
+/* VECTOR_ERASE_IF: Erase a value if a condition is true */
+#define VECTOR_ERASE_IF(vector, index, condition) \
+    do {                                          \
+        if (!vector) {                            \
+            MYERR("vector is NULL\n");            \
+            break;                                \
+        }                                         \
+        if (condition) {                          \
+            VECTOR_POP_AT(vector, index);         \
+        }                                         \
+    } while (0)
+
+/* VECTOR_ERASE_ALL_IF: Erase all values if a condition is true */
+#define VECTOR_ERASE_ALL_IF(vector, value, condition, compare) \
+    do {                                                       \
+        if (!vector) {                                         \
+            MYERR("vector is NULL\n");                         \
+            break;                                             \
+        }                                                      \
+        VECTOR_FOREACH(vector, it)                             \
+        if (condition) {                                       \
+            if (compare(vector->array[it], value)) {           \
+                VECTOR_POP_AT(vector, it);                     \
+                --it;                                          \
+            }                                                  \
+        }                                                      \
+    } while (0)
+
+/* VECTOR_SORT: Sort values in a vector */
+#define VECTOR_SORT(vector, compare)                               \
+    do {                                                           \
+        if (!vector) {                                             \
+            MYERR("vector is NULL\n");                             \
+            break;                                                 \
+        }                                                          \
+        for (size_t i = 0; i < vector->size; ++i) {                \
+            for (size_t j = i + 1; j < vector->size; ++j) {        \
+                if (compare(vector->array[i], vector->array[j])) { \
+                    VECTOR_SWAP(vector, i, j);                     \
+                }                                                  \
+            }                                                      \
+        }                                                          \
+    } while (0)
+
+#endif /* !VECTOR_H_ */
